7c8f0678615b20279a9faee99bede70a2d7b2528
LCTheo <34814044+LCTheo@users.noreply.github.com>
2021-02-01 16:31:08 +0100

    agent methode and Room constructor completion. Action attribute modification

--- src/main/java/agent/Agent.java
+++ src/main/java/agent/Agent.java
@@ -4,6 +4,7 @@ import environnement.environment;
 import environnement.Room;
 import environnement.Position;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -16,31 +17,60 @@ public abstract class Agent implements Runnable {
     protected Room[][] map;
     protected Position position;
 
-
-    private Queue<Action> plan;
+    protected Queue<Action> plan;
 
     protected Agent(environment environment){
-
+        sensor = new Sensor(environment);
+        effector = new Effector(environment);
+        updateState();
+        position = new Position();
+        position.x = (int) (Math.random()*4);
+        position.y = (int) (Math.random()*4);
+        plan = new LinkedList<>();
     }
 
     @Override
     public void run() {
-
-    }
-
-    private void move (){
-
-    }
-
-    private void gather(){
-
-    }
-
-    private void learning(){
-
+        while (true){
+            if (!plan.isEmpty()){
+                Action nextAction = plan.poll();
+                if(nextAction == Action.gather){
+                    effector.gather(position);
+                    map[position.x][position.y].setJewel(false);
+                }
+                else if(nextAction == Action.clean){
+                    effector.clean(position);
+                    map[position.x][position.y].setDust(false);
+                    map[position.x][position.y].setJewel(false);
+                }
+                else if(nextAction == Action.moveDown){
+                    position.y = position.y + 1;
+                    effector.move(position);
+                }
+                else if(nextAction == Action.moveLeft){
+                    position.x = position.x - 1;
+                    effector.move(position);
+                }
+                else if(nextAction == Action.moveRight){
+                    position.y = position.y + 1;
+                    effector.move(position);
+                }
+                else if(nextAction == Action.moveHigh){
+                    position.y = position.y - 1;
+                    effector.move(position);
+                }
+            }
+            else {
+                planning();
+            }
+        }
     }
 
     private void updateState(){
+        map = sensor.scanEnvironment();
+    }
+
+    private void learning(){
 
     }
 
