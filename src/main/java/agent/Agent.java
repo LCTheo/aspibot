5d1cb4ed5e42a67ede2b71061f455187c96cf9b6
LCTheo <34814044+LCTheo@users.noreply.github.com>
2021-02-02 11:10:11 +0100

    position modification

--- src/main/java/agent/Agent.java
+++ src/main/java/agent/Agent.java
@@ -16,16 +16,19 @@ public abstract class Agent implements Runnable {
     protected Effector effector;
     protected Room[][] map;
     protected Position position;
-
+    protected Position goal;
     protected Queue<Action> plan;
 
     protected Agent(environment environment){
         sensor = new Sensor(environment);
         effector = new Effector(environment);
         updateState();
-        position = new Position();
-        position.x = (int) (Math.random()*4);
-        position.y = (int) (Math.random()*4);
+
+        int x = (int) (Math.random()*4);
+        int y = (int) (Math.random()*4);
+        position = new Position(x, y);
+        effector.move(position);
+
         plan = new LinkedList<>();
     }
 
@@ -36,32 +39,36 @@ public abstract class Agent implements Runnable {
                 Action nextAction = plan.poll();
                 if(nextAction == Action.gather){
                     effector.gather(position);
-                    map[position.x][position.y].setJewel(false);
+                    map[position.getX()][position.getY()].setJewel(false);
                 }
                 else if(nextAction == Action.clean){
                     effector.clean(position);
-                    map[position.x][position.y].setDust(false);
-                    map[position.x][position.y].setJewel(false);
+                    map[position.getX()][position.getY()].setDust(false);
+                    map[position.getX()][position.getY()].setJewel(false);
                 }
                 else if(nextAction == Action.moveDown){
-                    position.y = position.y + 1;
+
+                    position.setX(position.getX() + 1);
                     effector.move(position);
                 }
                 else if(nextAction == Action.moveLeft){
-                    position.x = position.x - 1;
+                    position.setX(position.getX() - 1);
                     effector.move(position);
                 }
                 else if(nextAction == Action.moveRight){
-                    position.y = position.y + 1;
+                    position.setY(position.getY() + 1);
                     effector.move(position);
                 }
                 else if(nextAction == Action.moveHigh){
-                    position.y = position.y - 1;
+                    position.setY(position.getY() - 1);
                     effector.move(position);
                 }
             }
             else {
                 planning();
+            }
+            if ((position.getX() == goal.getX() && position.getY() == goal.getY()) && !map[goal.getX()][goal.getY()].isDust()){
+                goal = selectGoal();
             }
         }
     }
@@ -72,6 +79,10 @@ public abstract class Agent implements Runnable {
 
     private void learning(){
 
+    }
+
+    protected Position selectGoal(){
+        return new Position(1, 3);
     }
 
     protected abstract void planning();
--- src/main/java/environnement/Position.java
+++ src/main/java/environnement/Position.java
@@ -2,7 +2,28 @@ package environnement;
 
 public class Position {
 
-    public int x;
+    private int x;
 
-    public int y;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
