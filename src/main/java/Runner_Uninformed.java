001d4459d56443d9b73d00538e099d81965cccca
LCTheo <34814044+LCTheo@users.noreply.github.com>
2021-02-19 18:57:17 +0100

    .exe build and pom modification to build two jar and exe

---
+++ src/main/java/Runner_Uninformed.java
@@ -0,0 +1,23 @@
+import agent.Agent;
+import agent.InformedAgent;
+import agent.UninformedAgent;
+import environnement.Display;
+import environnement.Environment;
+
+import java.util.Locale;
+
+public class Runner_Uninformed {
+
+    public static void main(String[] args) {
+        Environment env = new Environment();
+        Agent agent = new InformedAgent(env);
+
+        Thread thread_env = new Thread(env);
+        Thread thread_agent = new Thread(agent);
+
+        thread_env.start();
+        thread_agent.start();
+
+        Display.init(env, "uninformed");
+    }
+}
